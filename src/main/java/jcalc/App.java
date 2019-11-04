213561cb98ddafe9c3d092f1cebbb2994bd67f64
Vyacheslav <supvlmail@gmail.com>
2019-11-04 18:17:20 +0500

    First commit

--- src/main/java/jcalc/App.java
+++ src/main/java/jcalc/App.java
@@ -12,27 +12,5 @@ public class App {
       } catch (IndexOutOfBoundsException e) {
         System.out.println("Вы не указали аргументы");
       }
-      /*
-        Pattern p = Pattern.compile("([I|V|X|L|C|D|M|0-9]+)\\s+[\\+|\\-|\\*|\\/]\\s+([I|V|X|L|C|D|M|0-9]+)");
-        Matcher m = p.matcher(args[0]);
-        if (m.find()) {
-          System.out.println("Вы ввели допустимое выражение!!!");
-          /*
-          try {
-            System.out.println(args[0]);
-            try {
-              System.out.println(NumberParser.parse(args[0]));
-            } catch (IllegalArgumentException e) {
-              // Обработка арабских цифр
-            } catch (IndexOutOfBoundsException e) {
-              System.out.println("Вы использовали по крайней мере одну цифру > 10");
-            }
-          } catch (IndexOutOfBoundsException e) {
-              System.out.println("Вы не указали аргументы");
-          }
-        } else {
-            System.out.println("Вы ввели недопустимое выражение!");
-        }
-    */
     }
 }
